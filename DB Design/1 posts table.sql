c1929010ecbf8b99d8e88c32abcd807659b5119c
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-09-09 15:49:19 +0530

    Update 1 posts table.sql

--- DB Design/1 posts table.sql
+++ DB Design/1 posts table.sql
@@ -2,7 +2,7 @@ create table posts(
 --    PostID
     pid number(5) primary key,
     description varchar2(100) not null,
-    owner_ph number(10) not null,
+    owner_ph number(12) not null,
     post_date date not null,
     upvotes number(38) not null,
     downvotes number(38) not null,
