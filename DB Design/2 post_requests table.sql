d34694e8d59133da4ad3d567e034d0105313d0c1
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-09-09 15:49:50 +0530

    Update 2 post_requests table.sql

--- DB Design/2 post_requests table.sql
+++ DB Design/2 post_requests table.sql
@@ -1,6 +1,6 @@
 create table post_requests(
 --    Post_ID
     pid number(5) primary key,
-    up_owners  number(10) ,
-    down_owners number(10)
+    up_owners  number(12) ,
+    down_owners number(12)
 );
