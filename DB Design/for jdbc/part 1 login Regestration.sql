17e3bb1435624869334da1ca1894ec84b50d27c0
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-09-09 18:10:09 +0530

    Update part 1 login Regestration.sql

--- DB Design/for jdbc/part 1 login Regestration.sql
+++ DB Design/for jdbc/part 1 login Regestration.sql
@@ -1,2 +1,2 @@
 --template
-insert into  POSTS values(1,'desc',999888254445,to_date('09/09/2017','dd/mm/yyyy'),0,0,null,null,0);
+insert into  POSTS values(1,'desc',999888254445,to_date('09/09/2017','dd/mm/yyyy'),0,0,null,null,0,'district','mandal');
